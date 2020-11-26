b1a59ae55028324e3ec7e2479ae43186fa4059b9
Dean Mikan <mikandean@gmail.com>
2020-11-26 16:50:18 +1100

    Added index file

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>Hi</div>
+</template>
+
+<script>
+// Vuex/services/mixins
+
+// Components
+
+// Third party plugins
+
+export default {
+  mixins: [],
+  components: {},
+  props: {},
+  validations: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+  provide() {
+    return {};
+  },
+  inject: [],
+};
+</script>
+
+<style lang="scss"></style>
